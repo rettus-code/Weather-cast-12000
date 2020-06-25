e6eb7a71de20e5f1f0e74a565328b387169ff73c
Rettus code <michael.rettusii@gmail.com>
2020-06-25 05:58:46 -0700

    Mostly done just need to incorporate local storage for history and icons

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Weather-cast-12000
 This will be my first project using an API
+
+Started with developing an object to capture all of the needed date from the weather API and store it for later display
+had issues with ajax working in the JS document, determined to be related to the jquery link was refferencing a slim
+version. Had an asynchronus issue when calling on uv data now fixed and I can currently create a complete object of
+current weather data. moving onto html formatting before expanding on future data.
--- script.js
+++ script.js
@@ -4,56 +4,75 @@ $(document).ready(function() {
       lat: null,
       long: null,
       conditions: "",
+      wind: "",
       temp: "",
       humid: "",
       uvI: "",
 
     }
+      https://rawgit.com/moment/moment/2.2.1/min/moment.min.js
       var userEntry = prompt("Enter zipcode or city name.");
-      console.log(userEntry);
-      var current = weather("weather");
-      //var future = weather("forecast");
+      var now = weather("weather");
+      var future = weather("forecast");
 
 
     function weather(state){
     var queryURL = "https://api.openweathermap.org/data/2.5/" + state + "?q="+userEntry +"&appid=209f024f18b94911ca5d243388fea797";
+    
       $.ajax({
           url: queryURL,
           method: "GET"
-        }).then(function(response) {         
-          wData.cityName = response.name;
-          wData.lat = response.coord.lat;
-          wData.long = response.coord.lon;       
-          wData.conditions = response.weather[0].description;
-          wData.temp =  (response.main.temp -273.15).toFixed(1);
-          wData.humid = response.main.humidity;
-          uvIndex(state);
-          //wData.uvI = uvi;
-          console.log(wData);
-        });         
+        }).then(function(response) { 
+          console.log(response);
+          if (state === "weather"){
+            var time = moment().format('llll');
+            $("#currCity").text(response.name+ "  ");
+            $("#currCity").append(time);
+            wData.lat = response.coord.lat;
+            wData.long = response.coord.lon;
+            $("#icon").append(iconSelector(response.weather[0].icon));       
+            $("#cWind").text("wind Speed: " + response.wind.speed +" mph");
+            $("#cTemp").text("Temperature: " + ((response.main.temp -273.15)/5*9 + 32).toFixed(1) +" F");
+            $("#cHum").text("Humidity: " + response.main.humidity +"%");
+            uvIndex(state);
+            current();
+        } else {
+          for (var i = 0; i < 5; i++){
+            var dt = response.list[i*8].dt_txt.split(" ")
+            $("#" + i + "card-header").text(dt[0]);
+            $("#" + i + "card-header").text(dt[0]);
+            $("#" + i + "card-header").text(dt[0]);
+            $("#" + i + "card-header").text(dt[0]);
+            var temp = response.list[i*8].main.temp-273.15;
+            $("#" + i + "temp").text((temp/5*9+32).toFixed(1) +" F");
+            $("#" + i + "temp").text((temp/5*9+32).toFixed(1) +" F");
+            $("#" + i + "temp").text((temp/5*9+32).toFixed(1) +" F");
+            $("#" + i + "temp").text((temp/5*9+32).toFixed(1) +" F");
+            var hum = response.list[i*8].main.humidity;
+            $("#" + i + "hum").text(hum +"%");
+            $("#" + i + "hum").text(hum +"%");
+            $("#" + i + "hum").text(hum +"%");
+            $("#" + i + "hum").text(hum +"%");
+          }
+        }
+    });
     }
      function uvIndex(state) {
       if (state === "weather"){
-        console.log(wData);
         queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=209f024f18b94911ca5d243388fea797&lat=" + wData.lat + "&lon="+wData.long;
-        console.log(queryURL);
         $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(respond) {
-          wData.uvI = respond.value;
-          console.log(respond);
+          $("#cUV").text("UV Index: " + respond.value);
       }) 
-    } else {
-      queryURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=209f024f18b94911ca5d243388fea797&lat=" + wData.lat + "&lon="+ wData.long + "&cnt=5";
-        console.log(queryURL);
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(respond) {
-          wData.uvI = respond.value;
-          console.log(respond);
-    })
     }
-  };
-})
+  }
+  function iconSelector(img){
+    var icon = $("<img>");
+    var iconUrl = "http://openweathermap.org/icon/wn/";   
+    icon.attr("src", iconUrl + img + "@2x.png");
+    return icon;
+}
+
+  })
